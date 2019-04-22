8528219512847880e91bfcd42f7aaf6bb7261b2d
a.baronskiy <a.baronskiy@mail.ru>
2019-04-22 16:46:47 +0300

    cors

--- main.go
+++ main.go
@@ -29,8 +29,21 @@ func callbackAnswer(c *gin.Context) {
 	c.String(http.StatusOK, "fa1fc205")
 }
 
+func CorsMiddlewareGin(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	c.Header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	c.Next()
+}
+
 func setupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(CorsMiddlewareGin)
+
 	router.POST("/callback", callbackAnswer)
 
 	return router
