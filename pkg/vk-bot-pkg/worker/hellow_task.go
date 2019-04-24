464d5dbd8f20b5226edb34598c57ae441d479205
a.baronskiy <a.baronskiy@mail.ru>
2019-04-24 12:18:48 +0300

    проверяю работоспособность

--- pkg/vk-bot-pkg/worker/hellow_task.go
+++ pkg/vk-bot-pkg/worker/hellow_task.go
@@ -17,6 +17,32 @@ type Hellow struct {
 }
 
 func (t *Hellow) Exec() {
+	// u, err := url.Parse("https://api.vk.com/method/messages.send")
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	// q := u.Query()
+	// q.Set("access_token", botconfig.ACCESSTOKEN)
+	// q.Set("message", "Hellow!")
+	// q.Set("random_id", strconv.Itoa(rand.Int()))
+	// q.Set("user_id", strconv.Itoa(t.Msg.FromId))
+	// q.Set("v", botconfig.VKVERSION)
+
+	// u.RawQuery = q.Encode()
+	// resp, err := http.Post(u.String(), "text/hmtl", nil)
+	// if err != nil {
+	// 	log.Println(err.Error())
+
+	// 	return
+	// }
+
+	// buf := new(bytes.Buffer)
+	// buf.ReadFrom(resp.Body)
+	// s := buf.String()
+
+	// fmt.Println(s)
+
 	u, err := url.Parse("https://api.vk.com/method/messages.send")
 	if err != nil {
 		log.Println(err.Error())
@@ -24,9 +50,9 @@ func (t *Hellow) Exec() {
 
 	q := u.Query()
 	q.Set("access_token", botconfig.ACCESSTOKEN)
-	q.Set("message", "Hellow!")
+	q.Set("message", "My king!")
 	q.Set("random_id", strconv.Itoa(rand.Int()))
-	q.Set("user_id", strconv.Itoa(t.Msg.FromId))
+	q.Set("user_id", botconfig.MYVKID)
 	q.Set("v", botconfig.VKVERSION)
 
 	u.RawQuery = q.Encode()
